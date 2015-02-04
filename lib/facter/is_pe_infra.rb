c7a2582e35a3d93c58e934603406dfa0fe79d560
Zachary Alex Stern <zacharyalexstern@gmail.com>
2015-02-03 17:44:40 -0800

    Refactor is_pe_infra fact.
    Now uses instances of package type instead of ridiculous `puppet
    resource` parsing.

--- lib/facter/is_pe_infra.rb
+++ lib/facter/is_pe_infra.rb
@@ -1,26 +1,23 @@
 require 'puppet'
-require 'facter/util/with_puppet'
 Facter.add('is_pe_infra') do
   confine :kernel => 'Linux'
-  with_puppet do
-    begin 
-      Module.const_get('Puppet')
-      setcode do
-        packages_pe = ['pe-puppetdb','pe-puppet-server','pe-puppet-dashboard-workers','pe-activemq','pe-httpd']
-        packages_all = Puppet::Type.type(:package).instances.map { |package| package.title }
-        packages_pe_installed = packages_all & packages_pe
+  begin 
+    Module.const_get('Puppet')
+    setcode do
+      packages_pe = ['pe-puppetdb','pe-puppet-server','pe-puppet-dashboard-workers','pe-activemq','pe-httpd']
+      packages_all = Puppet::Type.type(:package).instances.map { |package| package.title }
+      packages_pe_installed = packages_all & packages_pe
 
-        if packages_pe_installed.length == 0
-          false
-        else
-          true
-        end
-      end
-    rescue NameError => e
-      setcode do
-        # We assume true if the fact logic fails becaue if this returns nil when it shouldn't it will be Very Bad(tm) for you.
+      if packages_pe_installed.length == 0
+        false
+      else
         true
       end
+    end
+  rescue NameError => e
+    setcode do
+      # We assume true if the fact logic fails becaue if this returns nil when it shouldn't it will be Very Bad(tm) for you.
+      true
     end
   end
 end
