053c1886bfe776334292b2729dc50fa7bc460c64
Zachary Alex Stern <zacharyalexstern@gmail.com>
2015-02-05 15:23:35 -0800

    Test that ssldir and server classes are included
    Also, include dependency puppetlabs-inifile.

--- spec/classes/cutover_spec.rb
+++ spec/classes/cutover_spec.rb
@@ -20,5 +20,22 @@ describe 'cutover' do
       it { should_not contain_class('cutover::server') }
       it { should_not contain_class('cutover::ssldir') }
     end
+
+    describe 'should inlude cutover::server and cutover::ssldir if manage_server == true' do
+      let(:params) {{
+        :manage_server => true,
+        :server => 'foo.bar.com',
+      }}
+      let(:facts) {{
+        :kernel => 'Linux',
+        :is_pe_infra => false,
+      }}
+
+      it { should compile.with_all_deps }
+      
+      it { should_not contain_notify('pe-agents-only') }
+      it { should contain_class('cutover::server') }
+      it { should contain_class('cutover::ssldir') }
+    end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -25,6 +25,7 @@ RSpec.configure do |c|
     puppet_module_install(:source => proj_root, :module_name => 'cutover')
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
     end
   end
 end
