2c7dd761e3a00372df24c06781be2a60e295d654
Zee Alexander <zee@zee.space>
2016-02-11 16:43:16 -0800

    Add missing packages for PE and POSS.

--- lib/facter/is_puppet_infra.rb
+++ lib/facter/is_puppet_infra.rb
@@ -4,7 +4,16 @@ Facter.add('is_puppet_infra') do
   begin 
     Module.const_get('Puppet')
     setcode do
-      packages_infra = ['pe-puppetdb','pe-puppetserver','pe-puppet-dashboard-workers','pe-activemq','pe-httpd','puppetserver','puppetdb']
+      packages_infra = ['pe-puppetdb',
+                        'pe-puppetserver',
+                        'pe-puppet-dashboard-workers',
+                        'pe-activemq',
+                        'pe-httpd',
+                        'pe-console-services',
+                        'pe-nginx',
+                        'puppetserver',
+                        'puppetdb',
+                       ]
       packages_all = Puppet::Type.type(:package).instances.map { |package| package.title }
       packages_infra_installed = packages_all & packages_infra
 
